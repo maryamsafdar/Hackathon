9aea9cbaa23631796cacc476e3a1ad0ef78b1864
Maryam Safdar <139070864+maryamsafdar@users.noreply.github.com>
2024-03-08 23:38:58 +0500

    .

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -16,9 +16,9 @@ const Categories = () => {
                </div>
             </div>
          </div>
-         {/*categories strt from here*/}
+        
          <div className='grid grid-cols-2 lg:grid-cols-3 md:grid-cols-3 gap-4'> 
-         {/*categorie 1*/}
+        
          <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
             <Link href={"/T-shirts"}>
                <Image src="/images/t-shirts/f451baa75283c687b74c0ad0469e1179.jpg" alt="t-shirts" width={260} height={260} className='rounded-xl duration-500 group-hover:scale-125 ' />
@@ -31,51 +31,69 @@ const Categories = () => {
                </div>
             </Link>
          </div>
-         {/*category2*/}
          <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
-            <Link href={"/pants"}>
-               <Image src="/image/red.jpg" alt="topss" width={350} height={350} className='rounded-xl duration-500 group-hover:scale-125 ' />
-               <div className=' hidden lg:block absolute -bottom-24  group-hover:bottom-6 duration-500  scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>PANTS</h1>
+            <Link href={"/coats"}>
+               <Image src="/images/coat/Women's Blue Wool Trench Coat _ Trench Coat _ Wool Trench Coat.jpg " alt="coat" width={260} height={260} className='rounded-xl duration-500 group-hover:scale-125 ' />
+               
+               <div className=' hidden lg:block absolute -bottom-24 duration-500 group-hover:bottom-2 scroll-m-20 text-xl font-bold tracking-tight lg:text-2xl bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Coats</h1>
                </div>
-               <div className=' block lg:hidden absolute bottom-6 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>PANTS</h1>
+               <div className='block lg:hidden absolute bottom-8  duration-500 z-[100] scroll-m-20 text-2xl font-bold tracking-tight bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Coats</h1>
                </div>
             </Link>
          </div>
          <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
             <Link href={"/accessories"}>
-               <Image src="/image/ni.jpg" alt="topss" width={300} height={300} className='rounded-xl duration-500 group-hover:scale-125 ' />
-               <div className='hidden lg:block absolute -bottom-24  group-hover:bottom-6 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>ACCESSORIES</h1>
+               <Image src="/images/accessories/Promise Rings_ 22 The Most Popular Ring Ideas For Your Love.jpg" alt="accessories" width={260} height={260} className='rounded-xl duration-500 group-hover:scale-125 ' />
+               
+               <div className=' hidden lg:block absolute -bottom-24 duration-500 group-hover:bottom-2 scroll-m-20 text-xl font-bold tracking-tight lg:text-2xl bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Accessories</h1>
                </div>
-               <div className='block lg:hidden absolute bottom-6 duration-500 scrol-l-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>ACCESSORIES</h1>
-               </div>
-            </Link>-
-         </div>
-         <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
-            <Link href={"/shoes"}>
-               <Image src="/image/ss.jpg" alt="topss" width={300} height={300} className='rounded-xl duration-500 group-hover:scale-125 ' />
-               <div className='hidden lg:block absolute -bottom-24  group-hover:bottom-6 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>SHOES</h1>
-               </div>
-               <div className='block lg:hidden absolute:bottom-6 duration-500  scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>SHOES</h1>
+               <div className='block lg:hidden absolute bottom-8  duration-500 z-[100] scroll-m-20 text-2xl font-bold tracking-tight bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Accessories</h1>
                </div>
             </Link>
          </div>
          <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
             <Link href={"/shoes"}>
-               <Image src="/image/ss.jpg" alt="topss" width={300} height={300} className='rounded-xl duration-500 group-hover:scale-125 ' />
-               <div className='hidden lg:block absolute -bottom-24  group-hover:bottom-6 duration-500 scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>SHOES</h1>
+               <Image src="/images/shoes/Rhinestone Decor Point Toe Back Zipper Flats.jpg" alt="shoes" width={260} height={260} className='rounded-xl duration-500 group-hover:scale-125 ' />
+               
+               <div className=' hidden lg:block absolute -bottom-24 duration-500 group-hover:bottom-2 scroll-m-20 text-xl font-bold tracking-tight lg:text-2xl bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Shoes</h1>
                </div>
-               <div className='block lg:hidden absolute:bottom-6 duration-500  scroll-m-20 text-3xl font-extrabold tracking-tight bg-myWhite/70  text-myOrange w-f'>
-                  <h1 className='left-3'>SHOES</h1>
+               <div className='block lg:hidden absolute bottom-8  duration-500 z-[100] scroll-m-20 text-2xl font-bold tracking-tight bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Shoes</h1>
                </div>
             </Link>
          </div>
+         <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
+            <Link href={"purfumes"}>
+               <Image src="/images/purfumes/Lady million.jpg" alt="purfume" width={260} height={260} className='rounded-xl duration-500 group-hover:scale-125 ' />
+               
+               <div className=' hidden lg:block absolute -bottom-24 duration-500 group-hover:bottom-2 scroll-m-20 text-xl font-bold tracking-tight lg:text-2xl bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Purfumes</h1>
+               </div>
+               <div className='block lg:hidden absolute bottom-8  duration-500 z-[100] scroll-m-20 text-2xl font-bold tracking-tight bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Purfumes</h1>
+               </div>
+            </Link>
+         </div>
+         <div className='mb-7 lg:mb-0 overflow-hidden z-10 mx-auto group relative'>
+            <Link href={"/bags"}>
+               <Image src="/images/bags/GUESS_ Global Lifestyle Brand for Women, Men and Kids.jpg" alt="bag" width={260} height={260} className='rounded-xl duration-500 group-hover:scale-125 ' />
+               
+               <div className=' hidden lg:block absolute -bottom-24 duration-500 group-hover:bottom-2 scroll-m-20 text-xl font-bold tracking-tight lg:text-2xl bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Bags</h1>
+               </div>
+               <div className='block lg:hidden absolute bottom-8  duration-500 z-[100] scroll-m-20 text-2xl font-bold tracking-tight bg-gray-700/70 text-center text-white w-full'>
+                  <h1  className="uppercase">Bags</h1>
+               </div>
+            </Link>
+         </div>
+         
+        
+        
          </div>
          <div />
          <div />
